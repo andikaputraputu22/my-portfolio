3397d127efc7eeb5f0d5b6e69074031e4c573b2f
I Putu Andika Putra <andikaputraputu22@gmail.com>
2022-04-15 22:09:55 +0800

    Update projects code structure

--- src/components/Projectnya.js
+++ src/components/Projectnya.js
@@ -10,6 +10,93 @@ import {
 } from 'reactstrap';
 
 const Projectnya = () => {
+    
+    let projectList = []
+    let dataProject = [
+        {
+            photo: "info_vaksinasi.PNG",
+            title: "Info Vaksinasi",
+            description: "A simple website, that shows information about vaccination in Indonesia.",
+            link: "https://info-vaksinasi-indonesia.vercel.app/",
+            tag: ["React", "Web"],
+            type: 0
+        },
+        {
+            photo: "personal_website.PNG",
+            title: "Personal Website",
+            description: "My personal website. An easiest way to introduce myself to you and the world.",
+            link: "https://andika-portfolio.vercel.app/",
+            tag: ["React", "Web"],
+            type: 0
+        },
+        {
+            photo: "rsbedinfo_banner.jpg",
+            title: "Hospital Information App",
+            description: "A mobile application that help you to find out hospital information in Indonesia.",
+            link: "https://drive.google.com/file/d/1_VSZdE82O7dXQU9SY8gLt_uqRxJ6F2-Y/view?usp=sharing",
+            tag: ["Android", "Kotlin", "Mobile"],
+            type: -1
+        },
+        {
+            photo: "aguadigi_banner.jpg",
+            title: "Water Usage Monitoring App",
+            description: "A mobile application that can help you to monitor water usage and water bills.",
+            link: "#",
+            tag: ["Android", "Kotlin", "Mobile"],
+            type: 1
+        },
+        {
+            photo: "eticket_banner.jpg",
+            title: "Ticketing App",
+            description: "A mobile application that can help you to order tickets for the tourism object.",
+            link: "#",
+            tag: ["Android", "Java", "Mobile"],
+            type: 1
+        },
+        {
+            photo: "spkt_banner.jpg",
+            title: "Online Permission App",
+            description: "A mobile application that can help you to request a document according to your needs.",
+            link: "#",
+            tag: ["Android", "Java", "Mobile"],
+            type: 1
+        },
+        {
+            photo: "tbm_banner.jpg",
+            title: "Tour App",
+            description: "A mobile application that can help you to create your tour plan in Bali easily.",
+            link: "#",
+            tag: ["IOS", "Swift", "Mobile"],
+            type: 1
+        }
+    ]
+
+    dataProject.forEach((item, index) => {
+        projectList.push(
+            <Col lg="4">
+                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
+                    <CardBody className="py-5">
+                        <img
+                            className="img-fluid rounded portfolio-img mb-4"
+                            src={`/images/${item.photo}`}
+                            alt={item.title}
+                        />
+                        <h6 className="text-uppercase portfolio-title">{item.title}</h6>
+                        <p className="description mt-3">{item.description}</p>
+                        <div>
+                            {item.tag.map((item, index) => {
+                                return <Badge color="primary" pill className="mr-1">{item}</Badge>
+                            })}
+                        </div>
+                        <Button disabled={item.type === 1 ? true : false} className="mt-4" color="primary" href={item.link} target="_blank">
+                            {item.type === 0 ? "Show Demo" : item.type === -1 ? "Try Demo" : "Owned by Client"}
+                        </Button>
+                    </CardBody>
+                </Card>
+            </Col>
+        )
+    })
+
     return (
         <section id="section_project" className="section section-lg">
             <Container>
@@ -24,203 +111,7 @@ const Projectnya = () => {
                 <Row className="justify-content-center">
                     <Col lg="12">
                         <Row className="row-grid">
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/info_vaksinasi.PNG"
-                                            alt="Info Vaksinasi Indonesia"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Info Vaksinasi</h6>
-                                        <p className="description mt-3">
-                                            A simple website, that shows information about vaccination in Indonesia.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                React
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Web
-                                            </Badge>
-                                        </div>
-                                        <Button className="mt-4" color="primary" href="https://info-vaksinasi-indonesia.vercel.app/" target="_blank">
-                                            Show Demo
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/personal_website.PNG"
-                                            alt="My Personal Website"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Personal Website</h6>
-                                        <p className="description mt-3">
-                                            My personal website. An easiest way to introduce myself to you and the world.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                React
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Web
-                                            </Badge>
-                                        </div>
-                                        <Button className="mt-4" color="primary" href="https://andika-portfolio.vercel.app/" target="_blank">
-                                            Show Demo
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/rsbedinfo_banner.jpg"
-                                            alt="Hospital Information App"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Hospital Information App</h6>
-                                        <p className="description mt-3">
-                                            A mobile application that help you to find out hospital information in Indonesia.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Android
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Kotlin
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Mobile
-                                            </Badge>
-                                        </div>
-                                        <Button className="mt-4" color="primary" href="https://drive.google.com/file/d/1_VSZdE82O7dXQU9SY8gLt_uqRxJ6F2-Y/view?usp=sharing" target="_blank">
-                                            Try Demo
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/aguadigi_banner.jpg"
-                                            alt="Water Usage Monitoring App"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Water Usage Monitoring App</h6>
-                                        <p className="description mt-3">
-                                            A mobile application that can help you to monitor water usage and water bills.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Android
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Kotlin
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Mobile
-                                            </Badge>
-                                        </div>
-                                        <Button disabled className="mt-4" color="primary" href="#">
-                                            Owned by Client
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/eticket_banner.jpg"
-                                            alt="Ticketing App"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Ticketing App</h6>
-                                        <p className="description mt-3">
-                                            A mobile application that can help you to order tickets for the tourism object.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Android
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Java
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Mobile
-                                            </Badge>
-                                        </div>
-                                        <Button disabled className="mt-4" color="primary" href="#">
-                                            Owned by Client
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/spkt_banner.jpg"
-                                            alt="Online Permission App"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Online Permission App</h6>
-                                        <p className="description mt-3">
-                                            A mobile application that can help you to request a document according to your needs.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Android
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Java
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Mobile
-                                            </Badge>
-                                        </div>
-                                        <Button disabled className="mt-4" color="primary" href="#">
-                                            Owned by Client
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
-                            <Col lg="4">
-                                <Card className="card-lift--hover border-0 portfolio-card card-bottom-spacing">
-                                    <CardBody className="py-5">
-                                        <img
-                                            className="img-fluid rounded portfolio-img mb-4"
-                                            src="/images/tbm_banner.jpg"
-                                            alt="Tour App"
-                                        />
-                                        <h6 className="text-uppercase portfolio-title">Tour App</h6>
-                                        <p className="description mt-3">
-                                            A mobile application that can help you to create your tour plan in Bali easily.
-                                        </p>
-                                        <div>
-                                            <Badge color="primary" pill className="mr-1">
-                                                IOS
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Swift
-                                            </Badge>
-                                            <Badge color="primary" pill className="mr-1">
-                                                Mobile
-                                            </Badge>
-                                        </div>
-                                        <Button disabled className="mt-4" color="primary" href="#">
-                                            Owned by Client
-                                        </Button>
-                                    </CardBody>
-                                </Card>
-                            </Col>
+                            {projectList}
                         </Row>
                     </Col>
                 </Row>
